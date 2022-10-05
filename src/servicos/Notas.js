3bec156878da6e06dfa4aae1df0c016e60e2cab0
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-10-05 10:59:24 -0300

    Corrigindo bug

--- src/servicos/Notas.js
+++ src/servicos/Notas.js
@@ -1,29 +1,26 @@
 import { db } from "./SQLite"
-
 export function criaTabela() {
-    db.transaction((transaction) => {
-        transaction.executeSql("CREATE TABLE IF NOT EXISTS " +
-         "Notas " +
-         "(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, categotia TEXT, texto TEXT;)")
-    })
+  db.transaction((transaction) => {
+    transaction.executeSql("CREATE TABLE IF NOT EXISTS " +
+      "Notas " +
+      "(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, categoria TEXT, texto TEXT);")
+  })
 }
-
 export async function adicionaNota(nota) {
-    return new Promise((resolve) => {
-        db.transaction((transaction) => {
-            transaction.executeSql("INSERT INTO Notas (titulo, categoria, texto) VALUES (?, ?, ?);",
-            [nota.titulo, nota.categoria, nota.texto],
-            () => {resolve("Nota adicionada com sucesso!")})
-        })
+  return new Promise((resolve) => {
+    db.transaction((transaction) => {
+      transaction.executeSql("INSERT INTO Notas (titulo, categoria, texto) VALUES (?, ?, ?);", [nota.titulo, nota.categoria, nota.texto], () => {
+        resolve("Nota adicionada com sucesso!")
+      })
     })
+  })
 }
-
 export async function buscaNotas() {
-    return new Promise((resolve) => {
-        db.transaction((transaction) => {
-            transaction.executeSql("SELECT * FROM Notas;",
-            [],
-            (transaction, resultado) => {resolve(resultado.rows._array)})
-        })
+  return new Promise((resolve) => {
+    db.transaction((transaction) => {
+      transaction.executeSql("SELECT * FROM Notas;", [], (transaction, resultado) => {
+        resolve(resultado.rows._array)
+      })
     })
+  })
 }
